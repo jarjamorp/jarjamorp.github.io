16558aed9f308ddf6df78a6290e40842c9e363a8
jarjamorp <jarrahorphin@gmail.com>
2024-11-03 14:20:24 -0800

    clean python

--- _python/convert_to_webp.py
+++ _python/convert_to_webp.py
@@ -20,65 +20,6 @@ FOLDER_NAMES = [
 #     'stream-as-sculptor', 'surf', 'trash-treasure', 'veg-soy-sauce'
 # ]
 
-# def convert_to_webp(directory):
-#     counter = 0  # Initialize counter for prefixing filenames
-#     lst = os.listdir(directory)
-#     # lst.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))  # Sort ascending
-    
-#     # Check if the directory has any .webp files
-#     has_webp_files = any(filename.lower().endswith(".webp") for filename in lst)
-
-#     if has_webp_files:
-#         # Remove files that do not contain "-tn"
-#         lst = [filename for filename in lst if "-tn" in filename]
-#         for filename in lst:
-#             try:
-#                 file_root, file_extension = os.path.splitext(filename)
-#                 filepath = os.path.join(directory, filename)
-
-#                 # Check for the required extensions (both lowercase and uppercase)
-#                 if file_extension.lower() == ".webp":
-#                     with Image.open(filepath) as img:
-#                         # Resize the image maintaining the aspect ratio
-#                         aspect_ratio = img.height / img.width
-#                         new_height = int(THUMBNAIL_WIDTH * aspect_ratio)
-#                         thumbnail_img = img.resize((THUMBNAIL_WIDTH, new_height))
-                        
-#                         print("filepath", filepath)
-                        
-#                         # Save the thumbnail in .webp format with numbered prefix
-#                         thumbnail_webp_path = os.path.join(directory, f"{counter}-tn.webp")
-#                         thumbnail_img.save(thumbnail_webp_path, "WEBP")
-#                         print(f"Created thumbnail {thumbnail_webp_path[-10:]}")
-#                         counter += 1  # Increment counter for the next filename
-#             except Exception as e:
-#                 print(f"Error processing {filename}: {e}")
-#     else:
-#         for filename in lst:
-#             try:
-#                 file_root, file_extension = os.path.splitext(filename)
-#                 filepath = os.path.join(directory, filename)
-
-#                 # Check for the required extensions (both lowercase and uppercase)
-#                 if file_extension in [".jpg", ".jpeg", ".JPG", ".PNG", ".png"]:
-#                     with Image.open(filepath) as img:
-#                         # Resize the image maintaining the aspect ratio
-#                         aspect_ratio = img.height / img.width
-#                         new_height = int(THUMBNAIL_WIDTH * aspect_ratio)
-#                         thumbnail_img = img.resize((THUMBNAIL_WIDTH, new_height))
-                        
-#                         print("filepath", filepath)
-                        
-#                         # Save the image and thumbnail in .webp format with numbered prefix
-#                         large_webp_path = os.path.join(directory, f"{counter}.webp")
-#                         thumbnail_webp_path = os.path.join(directory, f"{counter}-tn.webp")
-#                         img.save(large_webp_path, "WEBP")
-#                         thumbnail_img.save(thumbnail_webp_path, "WEBP")
-#                         print(f"Converted {large_webp_path[-10:]} and {thumbnail_webp_path[-10:]}")
-#                         counter += 1  # Increment counter for the next filename
-#             except Exception as e:
-#                 print(f"Error processing {filename}: {e}")
-
 def convert_to_webp(directory):
     counter = 0  # Initialize counter for prefixing filenames
     lst = os.listdir(directory)
