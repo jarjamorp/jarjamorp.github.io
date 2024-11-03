d02d444bf96595b527e5e2dfd136c7770191d2cf
jarjamorp <jarrahorphin@gmail.com>
2024-11-03 14:19:47 -0800

    add experiments, panos, topanga-garden

--- _python/generate_gallery_item_lines.py
+++ _python/generate_gallery_item_lines.py
@@ -1,19 +1,20 @@
-def generate_gallery_items(number_of_items, filename):
+def generate_gallery_items(number_of_items, filename, seriesname):
     for i in range(number_of_items+1):
-        line = f'<div class="gallery__item"><a href="#img{i}"><img src="{filename}/{i}-tn.webp" alt="{filename.capitalize()}: {i}"></a></div>'
+        line = f'<div class="gallery__item"><a href="#img{i}"><img src="{filename}/{i}-tn.webp" alt="{seriesname}: {i}"></a></div>'
         print(line)
 
-def generate_lightbox_items(number_of_items, filename):
+def generate_lightbox_items(number_of_items, filename, seriesname):
     for i in range(number_of_items+1):
-        line = f'''    <div class="lightbox" id="img{i}">
+        line = f'''  <div class="lightbox" id="img{i}">
     <a href="#" class="lightbox__close">&times;</a>
-    <img src="{filename}/{i}.webp" alt="{filename.capitalize()}: {i}">
+    <img src="{filename}/{i}.webp" alt="{seriesname}: {i}">
   </div>'''
         print(line)
         
         
 # Call the function to generate and print the lines
-number_of_items = 38
-filename = 'experiments'
-# generate_gallery_items(number_of_items, filename)
-generate_lightbox_items(number_of_items, filename)
+number_of_items = 66
+filename = 'panos'
+seriesname = 'Long & Tall'
+# generate_gallery_items(number_of_items-1, filename, seriesname)
+generate_lightbox_items(number_of_items-1, filename, seriesname)
