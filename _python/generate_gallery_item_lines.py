d22a2f72f5f6084a0d7726f75d8e28b563756030
jarjamorp <jarrahorphin@gmail.com>
2024-11-02 23:33:58 -0700

    add experiments, refactoring

--- _python/generate_gallery_item_lines.py
+++ _python/generate_gallery_item_lines.py
@@ -1,17 +1,19 @@
-def generate_gallery_items(number_of_items):
+def generate_gallery_items(number_of_items, filename):
     for i in range(number_of_items+1):
-        line = f'<div class="gallery__item"><a href="#img{i}"><img src="b_w/{i}-tn.webp" alt="Image {i} - Black & White"></a></div>'
+        line = f'<div class="gallery__item"><a href="#img{i}"><img src="{filename}/{i}-tn.webp" alt="{filename.capitalize()}: {i}"></a></div>'
         print(line)
 
-def generate_lightbox_items(number_of_items):
+def generate_lightbox_items(number_of_items, filename):
     for i in range(number_of_items+1):
         line = f'''    <div class="lightbox" id="img{i}">
     <a href="#" class="lightbox__close">&times;</a>
-    <img src="b_w/{i}.webp" alt="Image{i} - Black & White">
+    <img src="{filename}/{i}.webp" alt="{filename.capitalize()}: {i}">
   </div>'''
         print(line)
         
         
 # Call the function to generate and print the lines
-# generate_gallery_items(98)
-generate_lightbox_items(98)
+number_of_items = 38
+filename = 'experiments'
+# generate_gallery_items(number_of_items, filename)
+generate_lightbox_items(number_of_items, filename)
